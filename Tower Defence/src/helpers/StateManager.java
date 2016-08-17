a8287129b791be7f08fd3ec27950823a5610e512
Dzoni <Nikola.Ratinac@gmail.com>
2016-08-17 16:13:00 +0200

    Uradjena funkcionalnost Play dugmeta.

--- Tower Defence/src/helpers/StateManager.java
+++ Tower Defence/src/helpers/StateManager.java
@@ -4,9 +4,28 @@ import data.Game;
 import data.MainMenu;
 
 public class StateManager {
+	
 	public static enum GameState {
 		MAINMENU, GAME
 	}
+	
+	static int map[][] = {
+			{0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0}, 
+			{0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,0,0},
+			{0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0},
+			{0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0},
+			{0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0},
+			{0,0,0,0,0,0,1,0,0,0,0,1,1,1,1,1,1,1,0,0},
+			{0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0},
+			{0,0,1,1,1,1,1,0,0,0,0,1,1,1,0,0,0,0,0,0},
+			{0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0},
+			{0,0,1,1,1,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0},
+			{0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	};
 	
 	public static GameState gameState = GameState.MAINMENU;
 	public static MainMenu mainMenu;
@@ -20,11 +39,17 @@ public class StateManager {
 			mainMenu.update();
 			break;
 		case GAME:
-			
+			if (game == null)
+				game = new Game(map);
+			game.update();
 			break;
 
 		default:
 			break;
 		}
+	}
+	
+	public static void setState(GameState newState){
+		gameState = newState;
 	}
 }
