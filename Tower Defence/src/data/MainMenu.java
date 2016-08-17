7a4f7468403c158984478fad778b89cebe6f667f
Dzoni <Nikola.Ratinac@gmail.com>
2016-08-17 15:34:57 +0200

    Konstruktor i update za MainMenu.

--- Tower Defence/src/data/MainMenu.java
+++ Tower Defence/src/data/MainMenu.java
@@ -2,13 +2,18 @@ package data;
 
 import org.newdawn.slick.opengl.Texture;
 
+import UI.UI;
+
 import static helpers.Artist.*;
+
 public class MainMenu {
 	
 	private Texture background;
-	
+	private UI menuUI;
 	public MainMenu(){
 		background = QuickLoad("mainmenu");
+		menuUI = new UI();
+		menuUI.addButton("Play", "playButton", WIDTH / 2 - 128, (int) (HEIGHT * 0.45f));
 	}
 	
 	public void update(){
