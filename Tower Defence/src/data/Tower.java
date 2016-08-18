ea9eba04ae12b360e2786fe6e5e26306ba64fb85
lzrav <lzrav@DESKTOP-SV0DEPS>
2016-08-18 19:03:47 +0200

    sve sredjeno + svetle toweri kad pucaju

--- Tower Defence/src/data/Tower.java
+++ Tower Defence/src/data/Tower.java
@@ -6,7 +6,7 @@ import static helpers.Clock.Delta;
 
 import java.util.ArrayList;
 
-public abstract class Tower implements Entity{
+public abstract class Tower implements Entity {
 
 	private float x, y, timeSinceLastShot, firingSpeed, angle;
 	private int width, height, damage, range;
@@ -15,7 +15,7 @@ public abstract class Tower implements Entity{
 	private ArrayList<Enemy> enemies;
 	private ArrayList<Projectile> projectiles;
 	private boolean targeted;
-	
+
 	public Tower(TowerType type, Tile startTile, ArrayList<Enemy> enemies) {
 		baseTexture = type.towerBase;
 		cannonTexture = type.towerCannon;
@@ -32,78 +32,80 @@ public abstract class Tower implements Entity{
 		this.timeSinceLastShot = 0;
 		this.projectiles = new ArrayList<Projectile>();
 	}
-		
-	//stavio sam da crta iz dva dela, on crta sam bbazu (izgleda)
-	
-	private Enemy acquireTarget(){
+
+	// stavio sam da crta iz dva dela, on crta sam bbazu (izgleda)
+
+	private Enemy acquireTarget() {
 		Enemy closest = null;
 		float closestDistance = 10000;
-		for(Enemy e: enemies) {
-			if(isInRange(e) && findDistance(e) < closestDistance) {
+		for (Enemy e : enemies) {
+			if (isInRange(e) && findDistance(e) < closestDistance) {
 				closest = e;
 				closestDistance = findDistance(e);
 			}
 		}
-		if(closest != null)
+		if (closest != null)
 			targeted = true;
 		return closest;
 	}
-	
+
 	private float findDistance(Enemy e) {
-		//zasto ne koristi rastojanje izmedju dve tacke, nesto mi je hm hm
+		// zasto ne koristi rastojanje izmedju dve tacke, nesto mi je hm hm
 		float xDistance = Math.abs(e.getX() - x);
 		float yDistance = Math.abs(e.getY() - y);
 		return xDistance + yDistance;
 	}
-	
+
 	private boolean isInRange(Enemy e) {
 		float xDistance = Math.abs(e.getX() - x);
 		float yDistance = Math.abs(e.getY() - y);
-		if(xDistance <= range && yDistance <= range)
+		if (xDistance <= range && yDistance <= range)
 			return true;
 		return false;
 	}
-	
-	private float calcAngle(){
-		double angleTemp = Math.atan2(target.getY() - y, target.getX()  - x);
+
+	private float calcAngle() {
+		double angleTemp = Math.atan2(target.getY() - y, target.getX() - x);
 		return (float) Math.toDegrees(angleTemp) - 90;
 	}
-	
+
 	public void shoot() {
 		timeSinceLastShot = 0;
-		projectiles.add(new Projectile(quickLoad("bullet"),target, x + TILE_SIZE/4, y + TILE_SIZE/4 ,16,16, 360, 40));
+		projectiles.add(
+				new Projectile(quickLoad("bullet"), target, x + TILE_SIZE / 4, y + TILE_SIZE / 4, 16, 16, 360, 40));
 	}
-	
+
 	public void updateEnemyList(ArrayList<Enemy> enemyList) {
-		enemies = enemyList;		
+		enemies = enemyList;
 	}
-	
+
 	public void update() {
-		if(!targeted) {
+		if (!targeted) {
 			target = acquireTarget();
 		}
-		
-		if(target == null || target.isAlive() == false)
+
+		if (target == null || target.isAlive() == false)
 			targeted = false;
-		
-		timeSinceLastShot += Delta();
-		if(timeSinceLastShot > firingSpeed)
-			shoot();
-		
-		for(Projectile p: projectiles) {
-			p.update();
+
+		if (targeted == true) {
+
+			timeSinceLastShot += Delta();
+			if (timeSinceLastShot > firingSpeed)
+				shoot();
+
+			for (Projectile p : projectiles) {
+				p.update();
+			}
+			angle = calcAngle() - 50;
+			draw();
 		}
-		angle = calcAngle() - 50;
-		draw();
 	}
-	
+
 	public void draw() {
 		drawQuadTex(baseTexture, x, y, width, height);
 		drawQuadTexRot(cannonTexture, x, y, width, height, angle);
 	}
-	
-	
-	
+
 	public float getX() {
 		return x;
 	}
@@ -135,7 +137,5 @@ public abstract class Tower implements Entity{
 	public void setHeight(int height) {
 		this.height = height;
 	}
-
-	
 
 }
