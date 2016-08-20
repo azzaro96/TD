20c87cc787c7ba93cbb2a3e01818133ab29dda93
lzrav <lzrav@DESKTOP-SV0DEPS>
2016-08-20 21:55:28 +0200

    purple tower i namesten countdown na najjacu poziciju mogucu

--- Tower Defence/src/data/Game.java
+++ Tower Defence/src/data/Game.java
@@ -34,14 +34,15 @@ public class Game {
 
 	private void updateUI() {
 		gameUI.draw();
-		gameUI.getMenu("Info").getMenuButton("Lives").setCaption(""+Player.Lives);
+		
 		gameUI.getMenu("Info").getMenuButton("Gold").setCaption(""+Player.Cash);
 		gameUI.getMenu("Info").getMenuButton("Wave").setCaption(""+player.getWaveManager().getWaveNumber());
+		gameUI.getMenu("Info").getMenuButton("Lives").setCaption(""+Player.Lives);
 		//gameUI.drawString(650 , 180, "Cash: " + Player.Cash);
 		//gameUI.drawString(650, 210, "Wave " + waveManager.getWaveNumber());
 		if(player.getWaveManager().getCurrentWave().getEnemyList().size() == 0){
 			int nextWaveIn = (int) (player.getWaveManager().getTimeBetweenWaves() - player.getWaveManager().getTimeSinceLastWave());
-			gameUI.drawString(650, 240, "Next wave in " + nextWaveIn);
+			gameUI.drawString(650, 240,"" + nextWaveIn);
 		}
 		if (Mouse.next()) {
 			boolean mouseClicked = Mouse.isButtonDown(0);
@@ -71,9 +72,11 @@ public class Game {
 		gameUI.getMenu("TowerPicker").addButton(new Button("Purple Tower","" + TowerType.purpleTower.getCost() , TextureBank.purpleTowerFull, 0, 0));
 		
 		gameUI.createMenu("Info", 640, 7*TILE_SIZE, TILE_SIZE, TILE_SIZE, 1, 0);
-		gameUI.getMenu("Info").addButton(new Button("Lives","" + Player.Lives, TextureBank.heart, 0, 0));
+		
+		
+		gameUI.getMenu("Info").addButton(new Button("Wave","" + player.getWaveManager().getWaveNumber(), TextureBank.creepRose, 0, 0));
 		gameUI.getMenu("Info").addButton(new Button("Gold","" + Player.Cash, TextureBank.coin, 0, 0));
-		gameUI.getMenu("Info").addButton(new Button("Wave","" + player.getWaveManager().getWaveNumber(), TextureBank.road, 0, 0));
+		gameUI.getMenu("Info").addButton(new Button("Lives","" + Player.Lives, TextureBank.heart, 0, 0));
 	}
 
 	public void update() {
